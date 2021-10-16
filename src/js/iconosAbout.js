d90d66cda0a6e0377b19718370cd4bb5d346ec95
Enzo Santilli <enzo135246@gmail.com>
2021-10-16 13:19:07 -0300

    actualización de repositorio

--- src/js/iconosAbout.js
+++ src/js/iconosAbout.js
@@ -1,11 +1,13 @@
 import codigo from '../assets/icons/codificacion.png';
 import libro from '../assets/icons/libro.png';
 import objetivo from '../assets/icons/objetivo.png';
+import pc from '../assets/icons/computadora.png';
 
 const iconosAbout = {
     "codigo":codigo,
     "libro":libro,
-    "objetivo":objetivo
+    "objetivo":objetivo,
+    "pc":pc
 }
 
 export default iconosAbout;
